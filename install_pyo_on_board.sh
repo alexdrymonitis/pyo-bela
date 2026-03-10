f77df4553ee887772f33d7ccb975d4dff44e6a75
alex <alex@debian.zte.com.cn>
2026-03-10 12:28:05 +0200

    update scripts

--- install_pyo_on_board.sh
+++ install_pyo_on_board.sh
@@ -7,7 +7,7 @@ echo "=== Downloading pyo-bela binaries. ==="
 wget -q https://github.com/alexdrymonitis/pyo-bela/releases/download/v1.0.6/dist-packages.tar.bz2
 echo "=== Extracting pyo-bela binaries. ==="
 tar xjf dist-packages.tar.bz2
-echo "=== Copying files to the BB. ==="
+echo "=== Copying files to /usr/local/lib ==="
 #scp -r -q dist-packages/ root@${ip}:/usr/local/lib/python3.11/
 cp -r dist-packages/* /usr/local/lib/python3.11/
 echo "=== Cleanup. ==="
